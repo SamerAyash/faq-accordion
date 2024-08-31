0cb372968f12d478bb1ac77beccda76b6bcfb043
SamerAyash <sam.ayash200@gmail.com>
2024-08-31 21:04:21 +0300

    First Commit

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function toggleFaq(item){
     var faq_number= item.getAttribute('data-faq');
     var status= item.getAttribute('data-status');
     var faqCard= document.getElementById("faq-"+faq_number);
-    var icon_plus= faqCard.querySelector(".icon #icon-plus");
-    var icon_minus= faqCard.querySelector(".icon #icon-minus");
+    var icon_plus= faqCard.querySelector(".icon .icon-plus");
+    var icon_minus= faqCard.querySelector(".icon .icon-minus");
     var desc= faqCard.querySelector(".desc");
 
     document.querySelectorAll(".faq-header")
@@ -11,10 +11,10 @@ function toggleFaq(item){
         e.setAttribute('data-status','close');
         e.parentElement.querySelector(".desc").classList.remove("block");
         e.parentElement.querySelector(".desc").classList.add("hidden");
-        e.querySelector(".icon #icon-plus").classList.add("block");
-        e.querySelector(".icon #icon-plus").classList.remove("hidden");
-        e.querySelector(".icon #icon-minus").classList.remove("block");
-        e.querySelector(".icon #icon-minus").classList.add("hidden");
+        e.querySelector(".icon .icon-plus").classList.add("block");
+        e.querySelector(".icon .icon-plus").classList.remove("hidden");
+        e.querySelector(".icon .icon-minus").classList.remove("block");
+        e.querySelector(".icon .icon-minus").classList.add("hidden");
     });
 
     if(status == "open"){
